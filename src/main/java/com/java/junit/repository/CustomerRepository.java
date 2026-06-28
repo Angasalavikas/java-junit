b3b229640720b4e41021a36edd5781ec713a2508
Angasalavikas <angasalavikas123@gmail.com>
2026-06-28 14:21:11 +0530

    initial commit junit

---
+++ src/main/java/com/java/junit/repository/CustomerRepository.java
@@ -0,0 +1,17 @@
+package com.java.junit.repository;
+
+import com.java.junit.model.Customer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerRepository {
+    private List<Customer> customers;
+     public CustomerRepository(){
+         this.customers = new ArrayList<>();
+     }
+     public Customer save(Customer customer){
+         this.customers.add(customer);
+         return customer;
+     }
+}
